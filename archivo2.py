32698f87d138aa2938d5bb5f9ad38b0e03b843c7
Cristian Hurtado <cristianhurtado06@gmail.com>
2021-06-03 18:11:18 -0500

    Adicion de segundo resultado en el archivo2

--- archivo2.py
+++ archivo2.py
@@ -3,6 +3,7 @@ def funcion(x,y):
 
     return 3*x+2*y
 
-resultado = funcion(6,7)
-print ('El resultado de la funcion es: ', resultado)
-print('Este es el archivo 2 de git')
+resultado1 = funcion(6,7)
+resultado2 = funcion(8,4)
+print ('El resultado  1 de la funcion es: ', resultado1)
+print ('El resultado  2 de la funcion es: ', resultado2)
